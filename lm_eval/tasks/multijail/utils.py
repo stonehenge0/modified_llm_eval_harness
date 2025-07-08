dad0bb694e22a141466f9d41ae33fe2aaf7ce8a4
stonehenge0 <em.stein29@gmail.com>
2025-07-08 09:55:53 +0200

    multijail

---
+++ lm_eval/tasks/multijail/utils.py
@@ -0,0 +1,13 @@
+# flatten the original data to one columns, since it initially has one column per language prompt.
+def flatten_multilingual_csv(dataset):
+    language_columns = ["en", "zh", "it", "vi", "ar", "ko", "th", "bn", "sw", "jv"]
+
+    new_rows = []
+    for i, row in enumerate(dataset):
+        # row should be a dict
+        for lang in language_columns:
+            prompt = row.get(lang, "")
+            if not isinstance(prompt, str) or prompt.strip() == "":
+                continue
+            new_rows.append({"prompt": prompt, "id": f"{i}_{lang}"})
+    return new_rows
