c1d53d35fa9cbbd25157b0f53daec64a164290c5
stonehenge0 <em.stein29@gmail.com>
2025-07-08 11:43:30 +0200

    doc to target

--- lm_eval/tasks/multijail/utils.py
+++ lm_eval/tasks/multijail/utils.py
@@ -17,3 +17,8 @@ def process_docs(dataset):
     # Convert the list back to a Dataset object
     dataset_out = Dataset.from_list(new_rows)
     return dataset_out
+
+
+def doc_to_target(doc):
+    """Dummy target. Lm eval requires a target, but we won't use it since we are generating completions and not evaluating over the framework here."""
+    return ""
